c184dd581abdb6b9d44d792c09ac97040a5fcb9b
whatwewant <tobewhatwewant@gmail.com>
2021-01-29 11:39:34 +0800

    chore(proxy): fix ctx.status for 304, but 304 not found in browser, it is weired

--- src/app/proxy.ts
+++ src/app/proxy.ts
@@ -32,10 +32,17 @@ export default async function proxy(ctx: Context) {
     files: (ctx.request as any).files,
   });
 
+  ctx.logger.log(`proxy status:`, response.status);
+
   response.headers.delete('content-security-policy');
+  response.headers.delete('strict-transport-security');
+  // response.headers.delete('cache-control');
+  
+  // ctx.logger.log(`proxy status:`, JSON.stringify(response.headers.raw(), null, 2));
+
   ctx.set(response.headers.raw() as any);
   ctx.set('access-control-allow-origin', '*');
   ctx.set('access-control-expose-headers', 'ETag, Link, Location, Retry-After, X-GitHub-OTP, X-RateLimit-Limit, X-RateLimit-Remaining, X-RateLimit-Used, X-RateLimit-Reset, X-OAuth-Scopes, X-Accepted-OAuth-Scopes, X-Poll-Interval, X-GitHub-Media-Type, Deprecation, Sunset');
-  ctx.staus = response.status;
+  ctx.status = response.status;
   ctx.body = response.body;
 }
