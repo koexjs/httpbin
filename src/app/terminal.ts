f9dc93d3ab31a8d98cebb062a20eae1a79a021c4
whatwewant <tobewhatwewant@outlook.com>
2021-12-11 15:12:30 +0800

    chore(terminal): use web-terminal

--- src/app/terminal.ts
+++ src/app/terminal.ts
@@ -1,138 +1,143 @@
 import { Server } from 'http';
-import doreamon from '@zodash/doreamon';
-import * as WebSocket from '@zodash/websocket';
+import { relay } from '@cliz/web-terminal/lib/core/relay';
 
-export interface Client extends WebSocket.Socket {
-  code?: string;
-  //
-  isServer?: boolean;
-  client?: WebSocket.Socket;
-  //
-  isClient?: boolean;
-  server?: WebSocket.Socket;
-}
+// import doreamon from '@zodash/doreamon';
+// import * as WebSocket from '@zodash/websocket';
 
-export interface Options {
-  port?: string;
-  path?: string;
-}
+// export interface Client extends WebSocket.Socket {
+//   code?: string;
+//   //
+//   isServer?: boolean;
+//   client?: WebSocket.Socket;
+//   //
+//   isClient?: boolean;
+//   server?: WebSocket.Socket;
+// }
 
-export function relay(options?: Options) {
-  const logger = doreamon.logger.getLogger('relay');
+// export interface Options {
+//   port?: string;
+//   path?: string;
+// }
 
-  const path = options?.path || '/';
+// export function relay(options?: Options) {
+//   const logger = doreamon.logger.getLogger('relay');
 
-  const app = new WebSocket.Server({
+//   const path = options?.path || '/';
+
+//   const app = new WebSocket.Server({
+//     path,
+//   });
+
+//   //
+//   const serverSockets = new Map<string, Client>();
+//   const clientSockets = new Map<string, Client>();
+
+//   app.on('connection', (_client) => {
+//     const socket = _client as unknown as Client;
+
+//     logger.log('socket connect:', socket.id);
+
+//     socket.on('disconnect', () => {
+//       logger.log('socket disconnect:', socket.id);
+
+//       if (socket.isServer) {
+//         serverSockets.delete(socket.code);
+
+//         // make client disconnect
+//         if (socket.client) {
+//           socket.client.disconnect();
+//         }
+//       } else if (socket.isClient) {
+//         clientSockets.delete(socket.id);
+
+//         // release server.client
+//         if (socket.server) {
+//           (socket.server as Client).client = null;
+//         }
+//       }
+//     });
+
+//     // (server) online: code => server:wait
+//     socket.on('online', ({ code }) => {
+//       socket.isServer = true;
+//       socket.code = code;
+
+//       logger.log(`[server][online] code: ${code}`);
+
+//       serverSockets.set(code, socket);
+//     });
+
+//     // (client) connect: code => client:request
+//     socket.on('connect', ({ code }) => {
+//       socket.isClient = true;
+//       socket.code = code;
+
+//       logger.log(`[client][connect] ${socket.id}: ${code}`);
+
+//       if (!serverSockets.has(code)) {
+//         const message = `code(${code}) is invalid`;
+//         logger.error(`[client][connect][error]`, message);
+
+//         socket.emit('error', message);
+//         socket.disconnect();
+//         return;
+//       }
+
+//       // bind socket: server <-> client: bind one-2-one
+//       const serverSocket = serverSockets.get(code);
+//       const clientSocket = socket;
+//       // if already bind client, should emit error, because
+//       if (serverSocket.client) {
+//         const message = `code(${code}) is used by others, you cannot use it`;
+//         clientSocket.emit('error', message);
+//         clientSocket.disconnect();
+//         return;
+//       }
+//       clientSocket.server = serverSocket;
+//       serverSocket.client = clientSocket;
+
+//       clientSockets.set(socket.id, socket);
+
+//       clientSocket.emit('connected');
+//       serverSocket.emit('connected');
+//     });
+
+//     // communite following
+
+//     // [client] => server
+//     socket.on('input', ({ command }) => {
+//       const serverSocket = socket.server;
+
+//       serverSocket.emit('input', {
+//         id: socket.id,
+//         command,
+//       });
+//     });
+
+//     // [server] => client
+//     socket.on('output', ({ id, data }) => {
+//       if (!clientSockets.has(id)) {
+//         const error = new Error(`invalid client: ${id}`);
+//         return socket.emit('error', error.message);
+//       }
+
+//       const clientSocket = clientSockets.get(id);
+//       clientSocket.emit('output', {
+//         data,
+//       });
+//     });
+//   });
+
+//   return app;
+// }
+
+export default async (path: string, server: Server) => {
+  const app = await relay({
     path,
-  });
-
-  //
-  const serverSockets = new Map<string, Client>();
-  const clientSockets = new Map<string, Client>();
-
-  app.on('connection', (_client) => {
-    const socket = _client as unknown as Client;
-
-    logger.log('socket connect:', socket.id);
-
-    socket.on('disconnect', () => {
-      logger.log('socket disconnect:', socket.id);
-
-      if (socket.isServer) {
-        serverSockets.delete(socket.code);
-
-        // make client disconnect
-        if (socket.client) {
-          socket.client.disconnect();
-        }
-      } else if (socket.isClient) {
-        clientSockets.delete(socket.id);
-
-        // release server.client
-        if (socket.server) {
-          (socket.server as Client).client = null;
-        }
-      }
-    });
-
-    // (server) online: code => server:wait
-    socket.on('online', ({ code }) => {
-      socket.isServer = true;
-      socket.code = code;
-
-      logger.log(`[server][online] code: ${code}`);
-
-      serverSockets.set(code, socket);
-    });
-
-    // (client) connect: code => client:request
-    socket.on('connect', ({ code }) => {
-      socket.isClient = true;
-      socket.code = code;
-
-      logger.log(`[client][connect] ${socket.id}: ${code}`);
-
-      if (!serverSockets.has(code)) {
-        const message = `code(${code}) is invalid`;
-        logger.error(`[client][connect][error]`, message);
-
-        socket.emit('error', message);
-        socket.disconnect();
-        return;
-      }
-
-      // bind socket: server <-> client: bind one-2-one
-      const serverSocket = serverSockets.get(code);
-      const clientSocket = socket;
-      // if already bind client, should emit error, because
-      if (serverSocket.client) {
-        const message = `code(${code}) is used by others, you cannot use it`;
-        clientSocket.emit('error', message);
-        clientSocket.disconnect();
-        return;
-      }
-      clientSocket.server = serverSocket;
-      serverSocket.client = clientSocket;
-
-      clientSockets.set(socket.id, socket);
-
-      clientSocket.emit('connected');
-      serverSocket.emit('connected');
-    });
-
-    // communite following
-
-    // [client] => server
-    socket.on('input', ({ command }) => {
-      const serverSocket = socket.server;
-
-      serverSocket.emit('input', {
-        id: socket.id,
-        command,
-      });
-    });
-
-    // [server] => client
-    socket.on('output', ({ id, data }) => {
-      if (!clientSockets.has(id)) {
-        const error = new Error(`invalid client: ${id}`);
-        return socket.emit('error', error.message);
-      }
-
-      const clientSocket = clientSockets.get(id);
-      clientSocket.emit('output', {
-        data,
-      });
-    });
-  });
-
-  return app;
-}
-
-export default (path: string, server: Server) => {
-  const app = relay({
-    path,
+    attachable: true,
   });
 
   app.attach(server);
+
+  console.log('terminal online');
 };
